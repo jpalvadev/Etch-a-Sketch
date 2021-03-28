183c52d7e7817437ac9dadabbf08f3f8be734462
Juan Pablo Alvarez <jpalvadev@gmail.com>
2021-03-28 00:52:22 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,96 @@
 const titleText = document.querySelectorAll('.title__svg path');
-// const radioBtn3 = document.querySelector(
-//   '.buttons-container__radio-btn--3 path'
-// );
 
-// const lineas = document.querySelectorAll(`.lineas path`);
-const lineas = document.querySelector('.lili');
+const colorLines = document.querySelector('.color-lines');
 
-console.log(titleText);
+const rangeSlider = document.querySelector('.range-slider__slider');
 
-titleText.forEach((letter, i) => {
-  letter.style.strokeDasharray = letter.getTotalLength();
-  letter.style.strokeDashoffset = letter.getTotalLength();
-  letter.style.animation = `title-animation 0.5s ease forwards ${
-    0.25 * i - 3
-  }s`;
+const canvas = document.querySelector('.grid-container__grid');
+
+const colorPicker = document.querySelector('.buttons-container__input-color');
+
+const clearAllBtn = document.querySelector('.clear-all');
+
+const radioBtns = document.querySelectorAll('input[type="radio"]');
+console.log(radioBtns);
+
+let canvasSize = 51;
+
+// Draw Etch-a-Sketch
+const drawTitle = () => {
+  titleText.forEach((letter, i) => {
+    letter.style.strokeDasharray = letter.getTotalLength();
+    letter.style.strokeDashoffset = letter.getTotalLength();
+    letter.style.animation = `title-animation 0.5s ease forwards ${
+      0.25 * i - 3
+    }s`;
+  });
+};
+drawTitle();
+
+rangeSlider.oninput = () => {
+  console.log(rangeSlider.value);
+};
+
+const updateCanvasSize = () => {
+  // More performance
+  let divsContainer = document.createDocumentFragment();
+
+  canvas.innerHTML = '';
+
+  canvas.style.gridTemplateRows = `repeat(${canvasSize}, 1fr)`;
+  canvas.style.gridTemplateColumns = `repeat(${canvasSize}, 1fr)`;
+
+  for (let i = 0; i < canvasSize * canvasSize; i++) {
+    const canvasDiv = document.createElement('div');
+    canvasDiv.classList.add('grid-container__grid-div');
+    divsContainer.appendChild(canvasDiv);
+    // console.log(i);
+  }
+  canvas.appendChild(divsContainer);
+};
+
+rangeSlider.addEventListener('change', () => {
+  console.log('levantaste');
+  canvasSize = rangeSlider.value;
+  updateCanvasSize();
 });
 
-// console.log(radioBtn3);
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 255);
+};
 
-// radioBtn3.style.strokeDasharray = radioBtn3.getTotalLength();
-// console.log(radioBtn3.getTotalLength());
+const getRandomColor = () => {
+  return `rgb(${getRandomNumber()},${getRandomNumber()},${getRandomNumber()})`;
+};
 
-// radioBtn3.style.strokeDashoffset = radioBtn3.getTotalLength();
-// radioBtn3.style.animation = `title-animation 5s ease forwards`;
+const paintDiv = (e) => {
+  const target = e.target.closest('.grid-container__grid-div');
 
-// console.log();
-lineas.style.strokeDasharray = lineas.getTotalLength();
+  // if (!e.target.closest('.grid-container__grid-div') || e.buttons !== 1) return;
+  if (!target || e.buttons !== 1) return;
+  // e.target.closest('.grid-container__grid-div').style.backgroundColor = colorPicker.value;
+  if (radioBtns[0].checked) target.style.backgroundColor = colorPicker.value;
 
-lineas.style.strokeDashoffset = lineas.getTotalLength();
-lineas.style.animation = `title-animation 2.5s ease-in-out forwards`;
+  if (radioBtns[3].checked) target.style.backgroundColor = getRandomColor();
+};
+
+canvas.addEventListener('mouseover', paintDiv);
+
+////////
+// COLOR
+const drawColorLines = () => {
+  colorLines.style.animation = ``;
+  colorLines.style.strokeDasharray = colorLines.getTotalLength();
+  colorLines.style.strokeDashoffset = colorLines.getTotalLength();
+  colorLines.style.animation = `title-animation 2.5s ease-in-out forwards`;
+};
+drawColorLines();
+
+colorPicker.addEventListener('change', function (e) {
+  colorLines.style.stroke = colorPicker.value;
+  drawColorLines();
+});
+
+clearAllBtn.addEventListener('click', updateCanvasSize);
+
+updateCanvasSize();
