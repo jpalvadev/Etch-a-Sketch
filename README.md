ae89321172e2d3c1dd482ac3269c3146faa1b313
Juan Pablo Alvarez <jpalvadev@gmail.com>
2021-03-29 04:33:46 -0300

    Done!

---
+++ README.md
@@ -0,0 +1,15 @@
+# Etch-a-Sketch
+
+This Project is part of The Odin Project's Curriculum
+
+- [Live Here](https://jpalvadev.github.io/rock-paper-scissors-lizard-spock-graphics/)
+
+## Things I learned - getting better at
+
+- SVG manipulation.
+
+- Customized elements (Radio buttons, range slider).
+
+- Event Listeners: The program accepts mouse, touch and pen/stylus inputs.
+
+- Use of small functions when possible.
--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
 const titleText = document.querySelectorAll('.title__svg path');
-
+const modalText = document.querySelectorAll('.modal__svg path');
+const modal = document.querySelector('.modal');
+const tools = document.querySelectorAll('.tools-container img');
 const colorLines = document.querySelector('.color-lines');
-
 const rangeSlider = document.querySelector('.range-slider__slider');
-
 const canvas = document.querySelector('.grid-container__grid');
-
-const colorPicker = document.querySelector('.buttons-container__input-color');
-
+const inputColor = document.querySelector('.buttons-container__input-color');
+inputColor.value = '#444444';
 const clearAllBtn = document.querySelector('.clear-all');
-
 const radioBtns = document.querySelectorAll('input[type="radio"]');
-console.log(radioBtns);
-
+const radioBtnContainer = document.querySelectorAll(
+  '.buttons-container__radio-btn-container'
+);
 let canvasSize = 51;
+const gridSizeText = document.querySelector(
+  '.grid-container__grid-size--right'
+);
 
-// Draw Etch-a-Sketch
+///////////////////////////////
+// DYNAMIC MEDIA QUERY DETECTOR
+let mediaqueryList = window.matchMedia('(orientation:portrait)');
+const mediaqueryDetector = (tall) => {
+  if (tall.matches) {
+    modal.classList.add('visible');
+    drawModalText();
+  } else {
+    modal.classList.remove('visible');
+  }
+};
+
+/////////////
+// ANIMATIONS
 const drawTitle = () => {
   titleText.forEach((letter, i) => {
     letter.style.strokeDasharray = letter.getTotalLength();
@@ -25,10 +40,35 @@ const drawTitle = () => {
     }s`;
   });
 };
-drawTitle();
 
+const drawModalText = () => {
+  modalText.forEach((word, i) => {
+    word.style.animation = ``;
+    word.style.strokeDasharray = word.getTotalLength();
+    word.style.strokeDashoffset = word.getTotalLength();
+    word.style.animation = `title-animation 1.5s linear forwards ${1.5 * i}s`;
+  });
+};
+
+const drawColorLines = () => {
+  colorLines.style.animation = ``;
+  colorLines.style.strokeDasharray = colorLines.getTotalLength();
+  colorLines.style.strokeDashoffset = colorLines.getTotalLength();
+  colorLines.style.animation = `title-animation 2.5s ease-in-out forwards`;
+};
+
+/////////
+// CANVAS
 rangeSlider.oninput = () => {
-  console.log(rangeSlider.value);
+  gridSizeText.textContent = `${rangeSlider.value} X ${rangeSlider.value}`;
+};
+gridSizeText.textContent = `${rangeSlider.value} X ${rangeSlider.value}`;
+
+const calcWidth = () => {
+  const canvasWidth = window
+    .getComputedStyle(canvas, null)
+    .getPropertyValue('height');
+  canvas.style.width = canvasWidth;
 };
 
 const updateCanvasSize = () => {
@@ -43,18 +83,15 @@ const updateCanvasSize = () => {
   for (let i = 0; i < canvasSize * canvasSize; i++) {
     const canvasDiv = document.createElement('div');
     canvasDiv.classList.add('grid-container__grid-div');
+
+    canvasDiv.style.backgroundColor = 'rgb(255, 255, 255)';
     divsContainer.appendChild(canvasDiv);
-    // console.log(i);
   }
   canvas.appendChild(divsContainer);
 };
 
-rangeSlider.addEventListener('change', () => {
-  console.log('levantaste');
-  canvasSize = rangeSlider.value;
-  updateCanvasSize();
-});
-
+//////////////
+// COLOR LOGIC
 const getRandomNumber = () => {
   return Math.floor(Math.random() * 255);
 };
@@ -63,34 +100,108 @@ const getRandomColor = () => {
   return `rgb(${getRandomNumber()},${getRandomNumber()},${getRandomNumber()})`;
 };
 
-const paintDiv = (e) => {
-  const target = e.target.closest('.grid-container__grid-div');
+const modifyColor = (target, command) => {
+  let colorArr = target.style.backgroundColor
+    .slice(4, -1)
+    .split(',')
+    .map((c) => {
+      if (command === 'lighten') return +c + 25;
+      else return +c - 25;
+    });
+  return `rgb( ${colorArr[0]}, ${colorArr[1]}, ${colorArr[2]})`;
+};
 
-  // if (!e.target.closest('.grid-container__grid-div') || e.buttons !== 1) return;
-  if (!target || e.buttons !== 1) return;
-  // e.target.closest('.grid-container__grid-div').style.backgroundColor = colorPicker.value;
-  if (radioBtns[0].checked) target.style.backgroundColor = colorPicker.value;
-
+const paintStyle = (target) => {
+  if (radioBtns[0].checked) target.style.backgroundColor = inputColor.value;
+  if (radioBtns[1].checked) {
+    target.style.backgroundColor = modifyColor(target, 'lighten');
+  }
+  if (radioBtns[2].checked) {
+    target.style.backgroundColor = modifyColor(target, 'darken');
+  }
   if (radioBtns[3].checked) target.style.backgroundColor = getRandomColor();
 };
 
-canvas.addEventListener('mouseover', paintDiv);
-
-////////
-// COLOR
-const drawColorLines = () => {
-  colorLines.style.animation = ``;
-  colorLines.style.strokeDasharray = colorLines.getTotalLength();
-  colorLines.style.strokeDashoffset = colorLines.getTotalLength();
-  colorLines.style.animation = `title-animation 2.5s ease-in-out forwards`;
+const setActiveTool = (e) => {
+  tools.forEach((tool) => tool.classList.remove('active'));
+  e.target.classList.add('active');
+  if (!tools[0].classList.contains('active')) {
+    radioBtnContainer.forEach((btn) => (btn.style.opacity = '0'));
+  } else radioBtnContainer.forEach((btn) => (btn.style.opacity = '1'));
 };
-drawColorLines();
 
-colorPicker.addEventListener('change', function (e) {
-  colorLines.style.stroke = colorPicker.value;
+///////////////
+// COLOR PICKER
+const rgbToHex = (rgb) => {
+  const hex = Number(rgb).toString(16);
+  if (hex.length < 2) hex = '0' + hex;
+  return hex;
+};
+const rgbArrayToHex = (rgb) => {
+  let colorArr = rgb.slice(4, -1).split(',');
+  return `#${rgbToHex(colorArr[0])}${rgbToHex(colorArr[1])}${rgbToHex(
+    colorArr[2]
+  )}`;
+};
+const getColorPicked = (e) => {
+  if (!tools[2].classList.contains('active')) return;
+  const divColor = e.target.style.backgroundColor;
+  colorLines.style.stroke = divColor;
+  inputColor.value = rgbArrayToHex(divColor);
   drawColorLines();
-});
+};
 
-clearAllBtn.addEventListener('click', updateCanvasSize);
+const getActiveTool = (target) => {
+  if (tools[0].classList.contains('active')) {
+    paintStyle(target);
+  }
+  if (tools[1].classList.contains('active')) {
+    target.style.backgroundColor = 'rgb(255, 255, 255)';
+  }
+};
 
-updateCanvasSize();
+const paintDivMouse = (e) => {
+  const target = e.target.closest('.grid-container__grid-div');
+  if (!target || e.buttons !== 1) return;
+  getActiveTool(target);
+};
+
+const paintDivTouch = (e) => {
+  e.preventDefault();
+  const changedTouch = e.changedTouches[0];
+  const target = document.elementFromPoint(
+    changedTouch.clientX,
+    changedTouch.clientY
+  );
+  if (!target.closest('.grid-container__grid-div')) return;
+  getActiveTool(target);
+};
+
+const init = () => {
+  tools.forEach((tool) => {
+    tool.addEventListener('click', setActiveTool);
+  });
+
+  rangeSlider.addEventListener('change', () => {
+    canvasSize = rangeSlider.value;
+    updateCanvasSize();
+  });
+  inputColor.addEventListener('change', function (e) {
+    colorLines.style.stroke = inputColor.value;
+    radioBtns[0].checked = true;
+    drawColorLines();
+  });
+  canvas.addEventListener('click', getColorPicked);
+  canvas.addEventListener('mouseover', paintDivMouse);
+  canvas.addEventListener('touchmove', paintDivTouch);
+  clearAllBtn.addEventListener('click', updateCanvasSize);
+  mediaqueryList.addEventListener('change', mediaqueryDetector);
+  updateCanvasSize();
+  calcWidth();
+  drawColorLines();
+  drawTitle();
+  window.onresize = calcWidth;
+  mediaqueryDetector(mediaqueryList);
+};
+
+init();
